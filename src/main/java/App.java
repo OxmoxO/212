8e055749c7809e513f5e9d0143f86bca16b3dd4e
oxmox <axxyssi@yandex.ru>
2022-04-01 11:17:21 +1000

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat kuzya = (Cat) applicationContext.getBean("cat");
+        Cat vasjka = (Cat) applicationContext.getBean("cat");
+        System.out.println(kuzya == vasjka);
+    }
+}
